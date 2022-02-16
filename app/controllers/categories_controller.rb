8914671543926c075e786a61d0e7a13b66923ecb
HENRY-KC <henrykc24@yahoo.com>
2022-02-16 11:47:00 +0100

    Add category addition form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,17 @@
 class CategoriesController < ApplicationController
   def index
     @categories = current_user.groups
+    puts params, '<<<<<>>>>>'
   end
-
   def show; end
 
-  def create; end
+  def create
+    puts params, '***********'
+    group = current_user.groups.create(name: params[:name], icon: params[:name])
+    if group.save
+      redirect_to user_categories_path, notice: 'Category added successfully'
+    else
+      redirect_to user_categories_path, alert: 'Oops! Category addition failed'
+    end
+  end
 end
