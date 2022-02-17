f0c4e493b755859ca8db128522ef862d890a8869
HENRY-KC <henrykc24@yahoo.com>
2022-02-17 16:57:03 +0100

    Add controller implementaion for addint entities

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -14,11 +14,19 @@ class EntitiesController < ApplicationController
 
   def new
     @transaction = Entity.new
-    @groups = current_user.groups.map(&:name)
+    # @groups = current_user.groups.map { |group| group.name}
+    @current_user = current_user
   end
 
   def create
-    puts 'groups=>', @groups
-    puts params
+    @entity = Entity.new(name: params[:name], amount: params[:amount].to_i, user_id: current_user.id)
+    if @entity.save
+      @group = current_user.groups.select { |group| group.name == params[:group]}
+      Entitygroup.create(entity_id: @entity.id, group_id: @group[0].id)
+    else
+      puts 'not ok'
+    end
+    
+    render :new
   end
 end
