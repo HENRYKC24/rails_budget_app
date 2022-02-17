020a4a05bc339f9e5fdcc9a74c3835fda45a284e
HENRY-KC <henrykc24@yahoo.com>
2022-02-17 14:28:41 +0100

    Fix linter error

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,28 +2,27 @@ class CategoriesController < ApplicationController
   def index
     @categories = current_user.groups
   end
+
   def show; end
-  
+
   def new
     @category = Group.new
   end
 
   def create
     uploaded_file = params[:group][:icon]
-    
+
     # puts uploaded_file.content_type[0, 5] === 'image', '^^^^'
-    
+
     @cat = nil
-    if uploaded_file.content_type[0, 5] === 'image'
-      uploaded_file.original_filename = ((Time.now.to_f * 1000).to_i).to_s + uploaded_file.original_filename
-      File.open(Rails.root.join('app/assets', 'images', uploaded_file.original_filename), 'wb') do |file|
-        file.write(uploaded_file.read)
-      end
+    if uploaded_file.content_type[0, 5] == 'image'
+      uploaded_file.original_filename = (Time.now.to_f * 1000).to_i.to_s + uploaded_file.original_filename
+      File.binwrite(Rails.root.join('app/assets', 'images', uploaded_file.original_filename), uploaded_file.read)
       @cat = Group.new(name: params[:group][:name], icon: uploaded_file.original_filename)
       @cat.user_id = current_user.id
     end
-     
-    if @cat 
+
+    if @cat
       if @cat.save
         redirect_to user_categories_path, notice: 'Category added successfully'
       else
@@ -34,7 +33,7 @@ class CategoriesController < ApplicationController
     end
   end
 
-  private 
+  private
 
   def category_param
     # params.required(:group).permit(:name, :icon)
--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -3,7 +3,7 @@ class EntitiesController < ApplicationController
     @entities = Entity.all
     puts 'All entities => ', @entities
     @entity_groups = Group.find(params[:category_id]).entitygroups
-    @entity_ids = @entity_groups.map { |group| group.entity_id }
+    @entity_ids = @entity_groups.map(&:entity_id)
     puts 'IDS', @entity_ids
     # puts 'Start', @entities, 'Middle', @entity_groups, 'End'
 
@@ -14,7 +14,7 @@ class EntitiesController < ApplicationController
 
   def new
     @transaction = Entity.new
-    @groups = (current_user.groups).map { |group| group.name}
+    @groups = current_user.groups.map(&:name)
   end
 
   def create
